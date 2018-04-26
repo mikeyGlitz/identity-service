e3c0af371f34183b8b588c3befb64973569e593b
Michael Glitzos <mglitzos@outlook.com>
2018-04-25 23:44:36 -0400

    Moved Test LDAP configuration to kotlin file

---
+++ src/test/kotlin/io/mikeyglitz/identity/config/TestConfig.kt
@@ -0,0 +1,50 @@
+package io.mikeyglitz.identity.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.*
+import org.springframework.core.env.Environment
+import org.springframework.core.io.ResourceLoader
+import org.springframework.data.ldap.repository.config.EnableLdapRepositories
+import org.springframework.ldap.core.LdapTemplate
+import org.springframework.ldap.core.support.LdapContextSource
+import org.springframework.ldap.test.TestContextSourceFactoryBean
+
+
+@Configuration
+@PropertySource("classpath:application-test.properties")
+@ComponentScan(basePackages = arrayOf("io.mikeyglitz.identity.**"))
+@EnableLdapRepositories(basePackages = arrayOf("io.mikeyglitz.identity.repository"))
+@Profile("test")
+class TestConfig {
+    @Autowired
+    private lateinit val environment: Environment
+    @Autowired
+    private lateinit val resourceLoader: ResourceLoader
+
+    @Bean
+    fun testContextSource(): TestContextSourceFactoryBean {
+        val contextSource = TestContextSourceFactoryBean()
+        contextSource.setDefaultPartitionName(environment!!.getRequiredProperty("ldap.partition.name"))
+        contextSource.setDefaultPartitionSuffix(environment.getRequiredProperty("ldap.partition.suffix"))
+        contextSource.setPassword(environment.getRequiredProperty("ldap.principal.password"))
+        contextSource.setPrincipal(environment.getRequiredProperty("ldap.principal.dn"))
+        contextSource.setPort(Integer.parseInt(environment.getRequiredProperty("ldap.port")))
+        contextSource.setLdifFile(resourceLoader!!.getResource(environment.getRequiredProperty("ldap.schema")))
+        return contextSource
+    }
+
+    @Bean
+    fun contextSource(): LdapContextSource {
+        val contextSource = LdapContextSource()
+        contextSource.setBase(environment!!.getRequiredProperty("ldap.partition.suffix"))
+        contextSource.setUrl(environment.getRequiredProperty("ldap.url"))
+        contextSource.userDn = environment.getRequiredProperty("ldap.principal.dn")
+        contextSource.password = environment.getProperty("ldap.principal.password")
+        return contextSource
+    }
+
+    @Bean
+    fun ldapTemplate(): LdapTemplate {
+        return LdapTemplate(this.contextSource())
+    }
+}
