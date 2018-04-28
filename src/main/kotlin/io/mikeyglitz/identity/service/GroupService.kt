20bae9eab148e023467ab3164bb1a52b5cdb2129
Michael Glitzos <mglitzos@outlook.com>
2018-04-28 16:38:32 -0400

    Added documentation to services

--- src/main/kotlin/io/mikeyglitz/identity/service/GroupService.kt
+++ src/main/kotlin/io/mikeyglitz/identity/service/GroupService.kt
@@ -5,18 +5,31 @@ import io.mikeyglitz.identity.model.User
 import io.mikeyglitz.identity.repository.GroupRepository
 import io.mikeyglitz.identity.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.ldap.support.LdapNameBuilder
-import org.springframework.ldap.support.LdapUtils
 import org.springframework.stereotype.Service
-import javax.naming.ldap.LdapName
 
+/**
+ * A service for accessing and manipulating data for groups
+ */
 @Service
 class GroupService {
+    /**
+     * The group repository
+     * This property allows groups to be queried or manipulated
+     */
     @Autowired
     private lateinit var groupRepository: GroupRepository
+    /**
+     * The user repository
+     * In this context, the user repository is only pulled in to query and find users
+     */
     @Autowired
     private lateinit var userRepository: UserRepository
 
+    /**
+     * Adds a user to a group
+     * @param username The name of the user to add to the group
+     * @param groupName The name of the group to add the user to
+     */
     fun addUserToGroup(username: String, groupName: String) {
         val user = userRepository.findByUsername(username)
         val group = groupRepository.findByName(groupName)
@@ -27,6 +40,11 @@ class GroupService {
         }
     }
 
+    /**
+     * Removes a user from a group
+     * @param username The name of the user to remove from the group
+     * @param groupName The name of the group to remove the user from
+     */
     fun removeUserFromGroup(username: String, groupName: String) {
         val user = userRepository.findByUsername(username)
         val group = groupRepository.findByName(groupName)
@@ -37,21 +55,49 @@ class GroupService {
         }
     }
 
+    /**
+     * Finds a group by name
+     * @param name The name to look for
+     * @return a group if one was found, null if a group was not found
+     */
     fun findGroup(name: String): Group? = groupRepository.findByName(name)
 
+    /**
+     * Creates a group
+     * When the group is created, the user that created the group will automatically be added to it.
+     * Automatically adding the user to the group was a design choice since the LDAP API did not allow
+     * for the creation of empty groups
+     * @param name The name of the group
+     * @param user The user that created the group. This user will be added as a member of the newly
+     * created group
+     */
     fun createGroup(name: String, user: User) {
         val group = Group(name)
         group.members.add(user.id)
         groupRepository.save(group)
     }
 
+    /**
+     * Deletes a group from the system
+     * @param name The name of the group to delete
+     */
     fun deleteGroup(name: String) {
         val group = groupRepository.findByName(name)
         groupRepository.delete(group)
     }
 
+    /**
+     * Lists all of the groups stored in the repository
+     * @return a list of groups, or no groups if the repository is currently empty
+     */
     fun allGroups(): List<Group> = groupRepository.findAll() as List<Group>
 
+    /**
+     * Modifies a group's information
+     * @param oldName The current name of the group
+     * @param newName The name to change the group name to
+     * @param description The new description for the group
+     */
     fun updateGroup(oldName: String, newName: String?, description: String?) {
         val group = groupRepository.findByName(oldName)
         if (group != null) {
--- src/main/kotlin/io/mikeyglitz/identity/service/UserService.kt
+++ src/main/kotlin/io/mikeyglitz/identity/service/UserService.kt
@@ -5,13 +5,35 @@ import io.mikeyglitz.identity.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+/**
+ * A service for accessing and manipulating information for users
+ */
 @Service
 class UserService {
+    /**
+     * The user repository
+     */
     @Autowired
     private lateinit var repository: UserRepository
 
+    /**
+     * A method which searches for user by username and password
+     * @param username The username to search for
+     * @param password The password to search for
+     * @return A boolean indicator to indicate whether the user was able to
+     * be found or not
+     */
     fun authenticate(username: String, password: String): Boolean =
         repository.findByUsernameAndPassword(username, password) != null
+
+    /**
+     * Creates a new user and adds the user to the LDAP directory
+     * @param username The user's name
+     * @param password The user's password
+     * @param firstName The user's first name
+     * @param lastName The user's last name
+     * @param email The user's email address
+     */
     fun create(
         username: String,
         password: String,
@@ -22,6 +44,15 @@ class UserService {
         val user = User(username, password, firstName, lastName, email)
         repository.save(user)
     }
+
+    /**
+     * Updates a user's information
+     * @param username The username to be updated
+     * @param password The password to set to the user's password
+     * @param firstName The user's new first name
+     * @param lastName The user's new last name
+     * @param email The user's new email address
+     */
     fun update(
         username: String,
         password: String?,
@@ -43,5 +74,11 @@ class UserService {
         }
     }
 
+    /**
+     * Performs a search for users by username
+     * This search will be case-insensitive and contain partial name matches
+     * @param name the name to search for
+     * @return a list of users, or an empty list if none were found
+     */
     fun search(name: String?): List<User> = repository.findByUsernameLikeIgnoreCase(name?:"*")
 }
